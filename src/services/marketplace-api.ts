6483b1b61277eec07ba9f16606407cdf396dff6b
Progress Aienobe <paienobe@gmail.com>
2023-09-22 15:02:43 +0100

    services restructure

--- src/services/marketplace-api.ts
+++ src/services/marketplace-api.ts
@@ -1,21 +1,41 @@
 import { BASE_API } from "../config";
 import { getQueryString } from "../utils";
 
+const makeApiRequest = async (
+  endpoint: string,
+  queryParams: Record<string, string | number>,
+  bearerToken: string
+) => {
+  const queryString = getQueryString(queryParams);
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${bearerToken}`,
+  };
+  const url = `${BASE_API}${endpoint}?${queryString}`;
+
+  try {
+    const response = await fetch(url, { method: "GET", headers });
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  } catch (error) {
+    // Handle the error appropriately (e.g., log it, throw a custom error, etc.)
+    throw error;
+  }
+};
+
 export const getCollection = async (
   contractAddress: string,
   chainId: number,
   bearerToken: string
 ) => {
   const queryParams = { contractAddress, chainId };
-  const queryString = getQueryString(queryParams);
-  const headers = {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${bearerToken}`,
-  };
-  const url = `${BASE_API}/marketplace-api/get-collection?${queryString}`;
-  const request = await fetch(url, { method: "GET", headers });
-  const response = await request.json();
-  return response;
+  return makeApiRequest(
+    "/marketplace-api/get-collection",
+    queryParams,
+    bearerToken
+  );
 };
 
 export const getCollectionNfts = async (
@@ -25,15 +45,11 @@ export const getCollectionNfts = async (
   bearerToken: string
 ) => {
   const queryParams = { contractAddress, chainId, page };
-  const queryString = getQueryString(queryParams);
-  const headers = {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${bearerToken}`,
-  };
-  const url = `${BASE_API}/marketplace-api/get-nfts-from-contract?${queryString}`;
-  const request = await fetch(url, { method: "GET", headers });
-  const response = await request.json();
-  return response;
+  return makeApiRequest(
+    "/marketplace-api/get-nfts-from-contract",
+    queryParams,
+    bearerToken
+  );
 };
 
 export const getSingleNft = async (
@@ -43,15 +59,7 @@ export const getSingleNft = async (
   bearerToken: string
 ) => {
   const queryParams = { contractAddress, tokenId, chainId };
-  const queryString = getQueryString(queryParams);
-  const headers = {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${bearerToken}`,
-  };
-  const url = `${BASE_API}/marketplace-api/get-nft?${queryString}`;
-  const request = await fetch(url, { headers });
-  const response = await request.json();
-  return response;
+  return makeApiRequest("/marketplace-api/get-nft", queryParams, bearerToken);
 };
 
 export const getCollectionTraits = async (
@@ -60,15 +68,11 @@ export const getCollectionTraits = async (
   bearerToken: string
 ) => {
   const queryParams = { contractAddress, chainId };
-  const queryString = getQueryString(queryParams);
-  const headers = {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${bearerToken}`,
-  };
-  const url = `${BASE_API}/marketplace-api/get-collection-traits?${queryString}`;
-  const request = await fetch(url, { headers });
-  const response = await request.json();
-  return response;
+  return makeApiRequest(
+    "/marketplace-api/get-collection-traits",
+    queryParams,
+    bearerToken
+  );
 };
 
 export const getNftOwner = async (
@@ -78,15 +82,11 @@ export const getNftOwner = async (
   bearerToken: string
 ) => {
   const queryParams = { contractAddress, tokenId, chainId };
-  const queryString = getQueryString(queryParams);
-  const headers = {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${bearerToken}`,
-  };
-  const url = `${BASE_API}/marketplace-api/get-nft-owners?${queryString}`;
-  const request = await fetch(url, { headers });
-  const response = await request.json();
-  return response;
+  return makeApiRequest(
+    "/marketplace-api/get-nft-owners",
+    queryParams,
+    bearerToken
+  );
 };
 
 export const getCollectionOwners = async (
@@ -95,13 +95,9 @@ export const getCollectionOwners = async (
   bearerToken: string
 ) => {
   const queryParams = { contractAddress, chainId };
-  const queryString = getQueryString(queryParams);
-  const headers = {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${bearerToken}`,
-  };
-  const url = `${BASE_API}/marketplace-api/get-collection-owners?${queryString}`;
-  const request = await fetch(url, { headers });
-  const response = await request.json();
-  return response;
+  return makeApiRequest(
+    "/marketplace-api/get-collection-owners",
+    queryParams,
+    bearerToken
+  );
 };
