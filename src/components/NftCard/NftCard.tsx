c5082a58e51f341fdac9c84c61824e46bedc389c
Progress Aienobe <paienobe@gmail.com>
2023-10-03 10:01:48 +0100

    tooltip on name overflow

--- src/components/NftCard/NftCard.tsx
+++ src/components/NftCard/NftCard.tsx
@@ -1,28 +1,54 @@
 import "./NftCard.css";
 import { Link } from "react-router-dom";
 import { TokenElement } from "../../types/reservoir-types/collection-nfts.types";
+import { useEffect, useRef, useState } from "react";
+import { useHomeContext } from "../../context/HomeContext/HomeContext";
+import CustomTooltip from "../CustomTooltip/CustomTooltip";
 
 type Props = { nft: TokenElement };
 
 const NftCard = ({ nft }: Props) => {
+  const { minimalCards } = useHomeContext()!;
+
+  const nameRef = useRef(null);
+  const [isOverflowing, setIsOverflowing] = useState(false);
+
   const currentEthAmount =
     nft?.market?.floorAsk?.price?.amount?.decimal?.toFixed(4);
   const symbol = nft?.market?.floorAsk?.price?.currency?.symbol;
   const currentValue = Math.ceil(nft?.market?.floorAsk?.price?.amount?.usd);
   const lastSale = nft?.market?.floorAsk?.price?.amount?.decimal?.toFixed(4);
+  const nftName = nft?.token?.name;
+  const nftId = nft?.token?.tokenId;
+
+  const nameLink = (
+    <Link to={`/nft/${nftId}`}>
+      <p ref={nameRef}>{nftName}</p>
+    </Link>
+  );
+
+  useEffect(() => {
+    if (nameRef.current) {
+      const element = nameRef.current as HTMLElement;
+      const isOverflowing = element.scrollWidth > element.clientWidth;
+      setIsOverflowing(isOverflowing);
+    }
+  }, [nameRef, minimalCards]);
 
   return (
     <div className="nft_card">
-      <Link to={`/nft/${nft?.token?.tokenId}`}>
+      <Link to={`/nft/${nftId}`}>
         <img src={nft?.token?.image} alt="" />
       </Link>
       <div className="nft_card_details">
         <div className="card_name">
-          <Link to={`/nft/${nft?.token?.tokenId}`}>
-            <p>{nft?.token?.name}</p>
-          </Link>
+          {isOverflowing ? (
+            <CustomTooltip text={nftName}>{nameLink}</CustomTooltip>
+          ) : (
+            <>{nameLink}</>
+          )}
         </div>
-        <Link to={`/nft/${nft?.token?.tokenId}`}>
+        <Link to={`/nft/${nftId}`}>
           <p className="nft_card_amount">
             {currentEthAmount && currentValue ? (
               <>
