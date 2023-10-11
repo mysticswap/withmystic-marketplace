baed53ec3e784dfe49777554a2e676460c4afbd8
Progress Aienobe <paienobe@gmail.com>
2023-10-11 14:21:27 +0100

    fixed filters bug on mobile

--- src/context/HomeContext/HomeContext.tsx
+++ src/context/HomeContext/HomeContext.tsx
@@ -1,12 +1,14 @@
 import { ReactNode, createContext, useContext, useState } from "react";
 import { HomeContextType, SelectedTrait } from "./types";
 import { defaultNumericFilters, dropdownOptions } from "../../constants";
+import { useIsMobile } from "../../hooks/useIsMobile";
 
 const HomeContext = createContext<HomeContextType | null>(null);
 
 type Props = { children: ReactNode };
 
 export const HomeContextProvider = ({ children }: Props) => {
+  const isMobile = useIsMobile();
   const [minimalCards, setMinimalCards] = useState(true);
   const [showFilters, setShowFilters] = useState(true);
   const [selectedTraits, setSelectedTraits] = useState<SelectedTrait[]>([]);
@@ -15,7 +17,7 @@ export const HomeContextProvider = ({ children }: Props) => {
   );
   const [isFetching, setIsFetching] = useState(false);
   const [numericFilters, setNumericFilters] = useState(defaultNumericFilters);
-  const [showMobileFilters, setShowMobileFilters] = useState(true);
+  const [showMobileFilters, setShowMobileFilters] = useState(!isMobile);
 
   return (
     <HomeContext.Provider
