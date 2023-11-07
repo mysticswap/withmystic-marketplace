f04d77237db4de7b82b92f53b6f612f4792db777
Progress Aienobe <paienobe@gmail.com>
2023-11-07 11:18:10 +0100

    added-preview

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -79,6 +79,8 @@ export const getHostName = () => {
       return "market.localhost.io";
     case "deploy-preview-6--heroic-duckanoo-b32f52.netlify.app":
       return "market.localhost.io";
+    case "deploy-preview-1--heroic-duckanoo-b32f52.netlify.app/":
+      return "market.localhost.io";
     default:
       return hostName;
   }
