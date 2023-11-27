d101de1b52352b59a11c58251050513ce4a3f5ca
Progress Aienobe <paienobe@gmail.com>
2023-11-27 18:15:18 +0100

    DEV-867-add-swaps-to-marketplace

---
+++ src/pages/SwapsPage/SwapsPage.tsx
@@ -0,0 +1,12 @@
+import "./SwapsPage.css";
+import { MysticSdk } from "mystic-sdk-1";
+
+const SwapsPage = () => {
+  return (
+    <div>
+      <MysticSdk token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZGQyMWQzMGUyMjkxNDdiNjY1ZGM3YSIsImlhdCI6MTY5MjIxMzcxNX0.2RZ8He93TUUMeW0t3aqpt5KxNlo1r1fjMNtaBYyP9rI" />
+    </div>
+  );
+};
+
+export default SwapsPage;
