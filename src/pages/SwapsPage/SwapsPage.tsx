23d828cf1df9b5d83e7ada3f6a478d64ea89dc84
Progress Aienobe <paienobe@gmail.com>
2023-11-28 11:13:28 +0100

    more-dynamic-styles

--- src/pages/SwapsPage/SwapsPage.tsx
+++ src/pages/SwapsPage/SwapsPage.tsx
@@ -3,7 +3,7 @@ import { MysticSdk } from "mystic-sdk-1";
 
 const SwapsPage = () => {
   return (
-    <div>
+    <div className="styles_page">
       <MysticSdk token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZGQyMWQzMGUyMjkxNDdiNjY1ZGM3YSIsImlhdCI6MTY5MjIxMzcxNX0.2RZ8He93TUUMeW0t3aqpt5KxNlo1r1fjMNtaBYyP9rI" />
     </div>
   );
