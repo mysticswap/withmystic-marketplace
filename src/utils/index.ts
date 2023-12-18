cbd4f5bfbaaf2043f07d2ba035f09d7e9a564805
Kelmat-ai <joaomffmoreira@gmail.com>
2023-12-18 15:05:18 +0000

    hangry

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -71,7 +71,7 @@ export const getHostName = () => {
   const hostName = window.location.hostname;
   switch (hostName) {
     case "localhost":
-      return "deploy-preview-44--heroic-duckanoo-b32f52.netlify.app";
+      return "deploy-preview-45--heroic-duckanoo-b32f52.netlify.app";
       return "market.localhost.io";
       return "marketplace.mysticswaplocalhost.io";
     case "deploy-preview-27--heroic-duckanoo-b32f52.netlify.app":
