dad3d48431b76e9d8bfe7f0541e5d3007fb83d97
Julio Patiño <franjp132@gmail.com>
2024-01-03 11:59:38 -0500

    add the host name for the preview in utils/index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -71,9 +71,10 @@ export const getHostName = () => {
   const hostName = window.location.hostname;
   switch (hostName) {
     case "localhost":
-      return "marketplace.mysticswaplocalhost.io";
+      return "https://deploy-preview-58--heroic-duckanoo-b32f52.netlify.app/";
       return "deploy-preview-40--heroic-duckanoo-b32f52.netlify.app";
-      return "market.localhost.io";
+    // return "marketplace.mysticswaplocalhost.io";
+    // return "market.localhost.io";
     case "deploy-preview-53--heroic-duckanoo-b32f52.netlify.app":
       return "marketplace.mysticswaplocalhost.io";
       return "deploy-preview-26--heroic-duckanoo-b32f52.netlify.app";
