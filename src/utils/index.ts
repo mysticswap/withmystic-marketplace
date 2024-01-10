cb7a46d90c5bc258667ce1915b5eb633fcf11d24
Olawale Shopeyin <shopeyinwale@gmail.com>
2024-01-10 19:09:36 +0100

    Fix test

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -76,7 +76,7 @@ export const getHostName = () => {
       return "market.localhost.io";
       return "marketplace.mysticswaplocalhost.io";
       return "market.localhost.io";
-    case "deploy-preview-69--heroic-duckanoo-b32f52.netlify.app":
+    case "deploy-preview-71--heroic-duckanoo-b32f52.netlify.app":
       return "marketplace.mysticswaplocalhost.io";
     // return "deploy-preview-26--heroic-duckanoo-b32f52.netlify.app";
     default:
