b5f021f68faf4117466456748f6dc7224125b433
Julio Patiño <franjp132@gmail.com>
2024-01-16 17:13:55 -0500

    add lazy loading to the pages components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { GlobalContextProvider } from "./context/GlobalContext/GlobalContext";
-import Home from "./pages/Home";
 import { HomeContextProvider } from "./context/HomeContext/HomeContext";
-import NftPage from "./pages/NftPage/NftPage";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-import ErrorPage from "./pages/ErrorPage/ErrorPage";
 import { NftPageContextProvider } from "./context/NftPageContext/NftPageContext";
 import { ConnectionContextProvider } from "./context/ConnectionContext/ConnectionContext";
 import { TransactionContextProvider } from "./context/TransactionContext/TransactionContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useEffect, useState } from "react";
+import { Suspense, lazy, useEffect, useState } from "react";
 import { getMarketplaceClient } from "./services/api/dynamic-system";
 import { ClientObject } from "./types/dynamic-system.types";
 import Loading from "./components/Loading/Loading";
 import { addLinks, changeStyles, updateMetadata } from "./utils/dynamic-styles";
 import { updateFavicon, updateSiteTitle } from "./utils";
-import SwapsPage from "./pages/SwapsPage/SwapsPage";
 import ReactGA from "react-ga";
+
+// Lazy Loading
+const Home = lazy(() => import("./pages/Home"));
+const NftPage = lazy(() => import("./pages/NftPage/NftPage"));
+const SwapsPage = lazy(() => import("./pages/SwapsPage/SwapsPage"));
+const ErrorPage = lazy(() => import("./pages/ErrorPage/ErrorPage"));
 
 const TRACKING_ID = import.meta.env.VITE_GOOGLE_ANALYTICS as string; // YOUR_OWN_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
@@ -61,23 +63,25 @@ function App() {
       <ConnectionContextProvider>
         <GlobalContextProvider client={client}>
           <TransactionContextProvider>
-            <Router>
-              <Navbar />
-              <Routes>
-                <Route
-                  path="/"
-                  element={<HomeContextProvider children={<Home />} />}
-                />
-                <Route
-                  path="/:contract/:id"
-                  element={<NftPageContextProvider children={<NftPage />} />}
-                />
-                <Route path="/swaps" element={<SwapsPage />} />
-                <Route path="*" element={<ErrorPage />} />
-              </Routes>
-              <Footer />
-              <ToastContainer limit={1} />
-            </Router>
+            <Suspense>
+              <Router>
+                <Navbar />
+                <Routes>
+                  <Route
+                    path="/"
+                    element={<HomeContextProvider children={<Home />} />}
+                  />
+                  <Route
+                    path="/:contract/:id"
+                    element={<NftPageContextProvider children={<NftPage />} />}
+                  />
+                  <Route path="/swaps" element={<SwapsPage />} />
+                  <Route path="*" element={<ErrorPage />} />
+                </Routes>
+                <Footer />
+                <ToastContainer limit={1} />
+              </Router>
+            </Suspense>
           </TransactionContextProvider>
         </GlobalContextProvider>
       </ConnectionContextProvider>
