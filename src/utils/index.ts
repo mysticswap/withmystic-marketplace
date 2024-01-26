a7f96b7b8608bb4108cd05e8de9ce3b28171934a
Julio Patiño <franjp132@gmail.com>
2024-01-26 10:07:23 -0500

    Steady Stack host

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -88,8 +88,8 @@ export const getHostName = () => {
       return "marketplace.mysticswaplocalhost.io";
       return "market.localhost.io";
     case "deploy-preview-77--heroic-duckanoo-b32f52.netlify.app":
-      return "deploy-preview-48--heroic-duckanoo-b32f52.netlify.app";
       return "deploy-preview-15--heroic-duckanoo-b32f52.netlify.app";
+      return "deploy-preview-48--heroic-duckanoo-b32f52.netlify.app";
       return "marketplace.mysticswaplocalhost.io";
       return "deploy-preview-25--heroic-duckanoo-b32f52.netlify.app";
     case "deploy-preview-69--heroic-duckanoo-b32f52.netlify.app":
