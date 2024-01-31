49413264ab7949d3578c62631bb53084dfe5461c
Julio Patiño <franjp132@gmail.com>
2024-01-30 19:42:07 -0500

    host preview

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -91,9 +91,9 @@ export const getHostName = () => {
       return "deploy-preview-25--heroic-duckanoo-b32f52.netlify.app";
       return "deploy-preview-19--heroic-duckanoo-b32f52.netlify.app";
       return "market.localhost.io";
-    case "deploy-preview-77--heroic-duckanoo-b32f52.netlify.app":
-      return "deploy-preview-15--heroic-duckanoo-b32f52.netlify.app";
+    case "deploy-preview-86--heroic-duckanoo-b32f52.netlify.app":
       return "deploy-preview-48--heroic-duckanoo-b32f52.netlify.app";
+      return "deploy-preview-15--heroic-duckanoo-b32f52.netlify.app";
       return "marketplace.mysticswaplocalhost.io";
       return "deploy-preview-25--heroic-duckanoo-b32f52.netlify.app";
     case "deploy-preview-81--heroic-duckanoo-b32f52.netlify.app":
