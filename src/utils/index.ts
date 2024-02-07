c0cc5066d5dc792085c43eaef7e6488ea3aebc11
Julio Patiño <franjp132@gmail.com>
2024-02-07 10:38:21 -0500

    remove lazy loading

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -84,11 +84,11 @@ export const getHostName = () => {
   switch (hostName) {
     case "localhost":
       return "marketplace.mysticswaplocalhost.io";
+      return "goldfinch.withmystic.xyz";
       //DiamondNXT-NFT host
       return "deploy-preview-48--heroic-duckanoo-b32f52.netlify.app";
       // SteadyStack host
       return "deploy-preview-15--heroic-duckanoo-b32f52.netlify.app";
-      return "Goldfinch.withmystic.xyz";
       return "deploy-preview-25--heroic-duckanoo-b32f52.netlify.app";
       return "deploy-preview-19--heroic-duckanoo-b32f52.netlify.app";
       return "market.localhost.io";
