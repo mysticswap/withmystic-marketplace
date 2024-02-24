9dfa12c1d9ebbb695072e536327510598fd4ee6f
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-02-25 04:05:20 +0900

    style collection page

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,6 +1,11 @@
 import "./Banner.css";
 import { useGlobalContext } from "../../context/GlobalContext/GlobalContext";
 import millify from "millify";
+import discordIcon from './../../assets/discord_icon.png'
+import xIcon from './../../assets/twitter-x-logo.png'
+import webSiteIcon from './../../assets/website-link-icon.png'
+import ethereumIcon from "./../../assets/ethereum-1.svg";
+
 import {
   collectionNetworkIcon,
   collectionsWithHiddenNames,
@@ -21,25 +26,9 @@ const Banner = ({details, activity, collectionInformation}:IProps) => {
     collectionChainId,
     client,
   } = useGlobalContext();
-  console.log({collectionMetadata})
-  const pillData = [
-    { title: "Items", value: collectionMetadata?.collections?.[0]?.tokenCount },
-    {
-      title: "Owners",
-      value: collectionMetadata?.collections?.[0]?.ownerCount,
-    },
-    {
-      title: "Volume",
-      value: collectionMetadata?.collections?.[0]?.volume?.allTime,
-      icon: collectionNetworkIcon[collectionChainId],
-    },
-    {
-      title: "Floor",
-      value:
-        collectionMetadata?.collections?.[0]?.floorAsk?.price?.amount?.native,
-      icon: collectionNetworkIcon[collectionChainId],
-    },
-  ];
+  console.log({collectionMetadata})              
+
+
   const collectionContract =
     collectionMetadata?.collections?.[0]?.primaryContract;
   const nameShouldBeHidden = collectionsWithHiddenNames[collectionContract!];
@@ -48,32 +37,6 @@ const Banner = ({details, activity, collectionInformation}:IProps) => {
     <div className="wrapper">
       <div className="banner">
         <img src={client.bannerUrl} alt="" />
-        {details && <div className="banner_details">
-          {!nameShouldBeHidden && (
-            <h1>{collectionMetadata?.collections?.[0]?.name}</h1>
-          )}
-          <div className="collection_pill">
-            {pillData.map((item) => {
-              return (
-                <div className="pill_item" key={item.title}>
-                  <div>
-                    {item.icon && (
-                      <img className="banner_pill_icon" src={item.icon} />
-                    )}
-                    <p>
-                      {Number(item.value) < 1
-                        ? Number(item.value)
-                        : Number(item.value)
-                        ? millify(Number(item.value))
-                        : "--"}
-                    </p>
-                  </div>
-                  <p>{item.title}</p>
-                </div>
-              );
-            })}
-          </div>
-        </div>}
         {activity && <div className="tabs">
           {tabOptions.map((item) => {
             return (
@@ -90,11 +53,52 @@ const Banner = ({details, activity, collectionInformation}:IProps) => {
           })}
         </div>}
       </div>
-        {collectionInformation && <div className="collection_information">
-          <span className="collection_name">{collectionMetadata?.collections?.[0]?.name}</span>
-          <span className="collection_info_label">Contract<span className="collection_info">{collectionMetadata?.collections?.[0]?.floorAsk.token.contract}</span></span>
-          <span className="collection_info_label">Created By<span className="collection_info">{collectionMetadata?.collections?.[0]?.floorAsk.maker}</span></span>
-          <span className="collection_info"></span>
+        {collectionInformation && details && <div className="collection_information_wrapper">
+          <div className="collection-information-container">
+            <img src={collectionMetadata?.collections?.[0]?.image} className="collection-main-image"/>
+            {!nameShouldBeHidden && <span className="collection_name">{collectionMetadata?.collections?.[0]?.name}</span>}
+            <div className="collection-info-container">
+              <span className="collection_info_label">Contract:{" "}<span className="collection_info">{collectionMetadata?.collections?.[0]?.floorAsk.token.contract}</span></span>
+              <span className="collection_info_label">Royalties:{" "}<span className="collection_info">{collectionMetadata?.collections?.[0]?.floorAsk.maker}</span></span>
+              <span className="collection_info_label">Created By:{" "}<span className="collection_info">{collectionMetadata?.collections?.[0]?.floorAsk.maker}</span></span>
+              <div className="social-icons-container">
+                <img className="social-icon" src={xIcon}></img>
+                <img className="social-icon" src={discordIcon}></img>
+                <img className="social-icon" src={webSiteIcon}></img>
+              </div>
+            </div>
+            <p className="collection-description">{collectionMetadata?.collections?.[0]?.description}</p>
+          </div>
+          <div className="items-information-container">
+              <div className="item-information">
+                <div className="item-information-value">{millify(Number(collectionMetadata?.collections?.[0]?.tokenCount))}</div>
+                <span className="item-information-label">Items</span>
+              </div>
+              <div className="item-information">
+                <div className="item-information-value">{millify(Number(collectionMetadata?.collections?.[0]?.ownerCount))}</div>
+                <span className="item-information-label">Owners</span>
+              </div>
+              <div className="item-information">
+                <div className="item-price-information">
+                  <img className="item-info-ethereum-icon" src={collectionNetworkIcon[collectionChainId]}></img>
+                  <span className="item-price-information-value">{millify(Number(collectionMetadata?.collections?.[0]?.volume?.allTime))}</span>
+                </div>
+                <span className="item-information-label">Total vol.</span>
+              </div>
+              <div className="item-information">
+                <div className="item-price-information">
+                  <img className="item-info-ethereum-icon" src={collectionNetworkIcon[collectionChainId]}></img>
+                  <span className="item-price-information-value">
+                    {Number(collectionMetadata?.collections?.[0]?.floorAsk?.price?.amount?.native) < 1
+                        ? Number(collectionMetadata?.collections?.[0]?.floorAsk?.price?.amount?.native)
+                        : Number(collectionMetadata?.collections?.[0]?.floorAsk?.price?.amount?.native)
+                        ? millify(Number(collectionMetadata?.collections?.[0]?.floorAsk?.price?.amount?.native))
+                        : "--"}
+                    </span>
+                  </div>
+                <span className="item-information-label">Floor price</span>
+              </div>
+            </div>
         </div>}
     </div>
   );
