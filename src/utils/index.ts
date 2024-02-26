ad2a40eb8da718ae2f5727935bd908314d2f5a04
Julio Patiño <franjp132@gmail.com>
2024-02-26 09:25:21 -0500

    add preview host

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -91,9 +91,9 @@ export const getHostName = () => {
       // SteadyStack host
       return "deploy-preview-15--heroic-duckanoo-b32f52.netlify.app";
       return "deploy-preview-25--heroic-duckanoo-b32f52.netlify.app";
-    case "deploy-preview-99--heroic-duckanoo-b32f52.netlify.app":
-      return "deploy-preview-14--heroic-duckanoo-b32f52.netlify.app";
+    case "deploy-preview-100--heroic-duckanoo-b32f52.netlify.app":
       return "marketplace.mysticswaplocalhost.io";
+      return "deploy-preview-14--heroic-duckanoo-b32f52.netlify.app";
     case "deploy-preview-81--heroic-duckanoo-b32f52.netlify.app":
       return "deploy-preview-48--heroic-duckanoo-b32f52.netlify.app";
       return "deploy-preview-19--heroic-duckanoo-b32f52.netlify.app";
