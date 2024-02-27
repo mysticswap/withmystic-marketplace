beaca016cbea2b1df8cb38be71b347c236466b06
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-02-27 23:37:01 +0900

    change mock collection image

--- src/pages/Home/Components/CollectionSection/CollectionsSection.tsx
+++ src/pages/Home/Components/CollectionSection/CollectionsSection.tsx
@@ -16,7 +16,7 @@ export const CollectionsSection = ({title, collections}:IProps) => {
     <div className="collections_section_grid">
       {collections.map((collection)=>
             <div className="collection_card_container">
-            <img className="collection_card_image" src="https://s3-alpha-sig.figma.com/img/3cab/5061/594142703370d867de77470166ece1a0?Expires=1708905600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ZqXxwuQyWpbd9MdyT4UyqAqkZ2R~qq~Fhyv1PXNQD-qegTW3guaD0AmzKpUL9sCQGDmybFPHGOm9R6-dz7oXAyWxwAZacZIptLutLg~1Gf-8~AeeIxFvV7udg0imZ3SAG7F9VNEJYK0WtSVgI4wS1-hyM80ocMv9txR8t0NnHeJ4l~4xUgYnrm-rqK5s8EWqjkgNSHx5KR~oYaf4Iubw7omwUeBiALvzd7VFSNGFb6IN0QNAEUXLVk8HhyYcGL-8Y7pi~h907qH394WLZvjeY1GVmU~8vPFkPeScudew4CPsTzU0Y9~uRyQBxAGu0yJx7E1ZprhFf2Ebs~pofzxHhQ__"/>
+            <img className="collection_card_image" src="https://i.seadn.io/gcs/files/6d4b873bd01107ff35489dbe69e031d1.png?auto=format&dpr=1&w=3840"/>
             <div className="collection_card_footer">
             <div className='collection_name_container'>
               <img className="check_icon" src={checkIcon}/>
