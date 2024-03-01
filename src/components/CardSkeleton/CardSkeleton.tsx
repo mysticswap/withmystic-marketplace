074d5ee8327aefb0e3cab158e06619df7043e210
Julio Patiño <franjp132@gmail.com>
2024-03-01 11:52:15 -0500

    add gray Theme in the CardSkeleton component

--- src/components/CardSkeleton/CardSkeleton.tsx
+++ src/components/CardSkeleton/CardSkeleton.tsx
@@ -1,4 +1,4 @@
-import Skeleton from "react-loading-skeleton";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "./CardSkeleton.css";
 import "react-loading-skeleton/dist/skeleton.css";
 
@@ -9,12 +9,14 @@ const CardSkeleton = ({ cards }: props) => {
   return Array(cards)
     .fill(0)
     .map((_item, index) => (
-      <div className="nft_card_skeleton" key={index}>
-        <Skeleton className="skeleton_img" />
-        <Skeleton className="text_skeleton" width={"90%"} />
-        <Skeleton className="text_skeleton" width={"40%"} />
-        <Skeleton className="text_skeleton" width={"40%"} />
-      </div>
+      <SkeletonTheme baseColor="#202020" highlightColor="#444">
+        <div className="nft_card_skeleton" key={index}>
+          <Skeleton className="skeleton_img" />
+          <Skeleton className="text_skeleton" width={"90%"} />
+          <Skeleton className="text_skeleton" width={"40%"} />
+          <Skeleton className="text_skeleton" width={"40%"} />
+        </div>
+      </SkeletonTheme>
     ));
 };
 export default CardSkeleton;
