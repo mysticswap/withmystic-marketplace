68225dc0c5c384bc1330654a6dd352b16ad68f63
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-03-04 22:34:07 +0900

    improve height of banner in collections page

--- src/pages/Collections/Collections.tsx
+++ src/pages/Collections/Collections.tsx
@@ -36,7 +36,7 @@ const CollectionsPage = () => {
     
   return (
     <div className="collections_page_container">
-      <Banner activity={false} displayCollectionAvatar={false} />
+      <Banner activity={false} displayCollectionAvatar={false} height={70} />
       <div className="collections_tabs_container">
         <Tab />
         <PeriodSelector/>
