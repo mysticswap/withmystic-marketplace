ad13ffb78be358c0fdae3d5718c40f1cbfa833f6
Julio Patiño <franjp132@gmail.com>
2024-03-12 11:54:10 -0500

    add more styles to the list view

--- src/components/ListAttributes/ListAttributes.tsx
+++ src/components/ListAttributes/ListAttributes.tsx
@@ -3,13 +3,14 @@ import "./ListAttributes.css";
 const ListAtributes = () => {
   return (
     <div className="list_attributes_container">
-      <p></p>
-      <p>NAME</p>
-      <p>RARITY</p>
-      <p>BUY NOW</p>
-      <p>LAST SALE</p>
-      <p>TOP BID</p>
-      <p>OWNER</p>
+      <p className="name_atribute">Name</p>
+      <div className="extra_atributes">
+        <p>Rarity</p>
+        <p>Buy Now</p>
+        <p>Last Sale</p>
+        <p>Top Bid</p>
+        <p>Owner</p>
+      </div>
     </div>
   );
 };
