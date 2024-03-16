1639fa419e74539c3bc0fe501d32ccc31d65c2f0
John Agbanusi <agbanusijohn@gmail.com>
2024-03-16 23:25:15 +0100

    re-add files

---
+++ src/components/ActivityFilterButton/AuctionChoiceButton.tsx
@@ -0,0 +1,31 @@
+// import { IconType } from "react-icons";
+import "./ActivityFilterButton.css";
+import Checkbox from "../Checkbox/Checkbox";
+import { useState } from "react";
+import { useGlobalContext } from "../../context/GlobalContext/GlobalContext";
+
+const AuctionButton = ({ disabled }: { disabled: boolean }) => {
+  const { setIsAuction, setCurrentToken } = useGlobalContext();
+  const [isClicked, setIsClicked] = useState(false);
+
+  const toggleSelection = () => {
+    setIsClicked(!isClicked);
+    setIsAuction(!isClicked);
+    setCurrentToken(0);
+  };
+
+  return (
+    <div
+      className="auction_filter_button"
+      onClick={() => !disabled && toggleSelection()}
+    >
+      <div>
+        <Checkbox isClicked={isClicked} />
+        <p>Auction to the highest bidder</p>
+        {/* <activity.icon display="block" size={20} /> */}
+      </div>
+    </div>
+  );
+};
+
+export default AuctionButton;
---
+++ src/components/CardSkeleton/CardSkeleton.tsx
@@ -0,0 +1,33 @@
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "./CardSkeleton.css";
+import "react-loading-skeleton/dist/skeleton.css";
+import { useGlobalContext } from "../../context/GlobalContext/GlobalContext";
+
+type props = {
+  cards: number;
+};
+const CardSkeleton = ({ cards }: props) => {
+  const { listView } = useGlobalContext();
+  return Array(cards)
+    .fill(0)
+    .map((_item, index) => (
+      <div
+        className={`${!listView ? "nft_card_skeleton" : "nft_list_skeleton"}`}
+        key={index}
+      >
+        <SkeletonTheme baseColor="#d9e0ec3d" highlightColor="#d9e0ec76">
+          {!listView && <Skeleton className="skeleton_img" />}
+          <Skeleton
+            className="text_skeleton"
+            width={`${!listView ? "90%" : "95%"}`}
+          />
+          <Skeleton
+            count={2}
+            className="text_skeleton"
+            width={`${!listView ? "40%" : "95%"}`}
+          />
+        </SkeletonTheme>
+      </div>
+    ));
+};
+export default CardSkeleton;
